8ff779270f4640296e3440040adc12dddf5a6c78
Rowan <48519289+scar055@users.noreply.github.com>
2025-12-10 11:30:47 +0100

    validate html gemaakt

---
+++ form/script.js
@@ -0,0 +1,8 @@
+// Stap 1: zoek de input en stop die in een variabele
+let inputElement = document.querySelector("input");
+
+// Stap 2: luister naar het invalid event, en wacht tot dat afgevuurd wordt..
+inputElement.addEventListener("invalid", function (ev) {
+  // Stap 3: pas de tekst in de validatiemelding aan
+  inputElement.setCustomValidity("Dit veld mag niet leeg zijn hoor..");
+});
